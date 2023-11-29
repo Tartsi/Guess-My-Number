89e959ffc0e55d36c06c8f40449f1d9236d55b9b
Tarvo Lilja <tarvo.lilja@hotmail.fi>
2023-11-29 18:39:52 +0200

    Setup, style header

--- src/routes.py
+++ src/routes.py
@@ -1,2 +1,21 @@
 from flask import render_template, request, redirect
 from app import app
+
+
+@app.route('/')
+def index():
+    # GET HIGHSCORE FROM DATABASE.
+    pass
+    return render_template('index.html')
+
+
+@app.route('/highscore', methods=['GET', 'POST'])
+def highscore():
+
+    if request.method == 'GET':
+
+        # GET HIGHSCORE FROM DATABASE.
+        pass
+
+    if request.method == 'POST':
+        pass
